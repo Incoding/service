34df9041f3ae0b924f9b66407dbe645c27ba5c4d
incoding <393630057@163.com>
2014-08-08 10:02:03 +0800

    测试session 和cookie得配合

--- src/main/java/com/aicai/service/biz/auth/login/UserInfo.java
+++ src/main/java/com/aicai/service/biz/auth/login/UserInfo.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * 测试获得外网ip 
@@ -18,7 +19,24 @@ public class UserInfo extends HttpServlet {
 	public void getUserInfo(HttpServletRequest req, HttpServletResponse resp) {
 		// System.out.println("getUserInfo==>");
 		String ip = req.getRemoteAddr();
+        dealSession(req, resp);
 		System.out.println(ip);
+    }
+
+    /**测试session与cookie得配合
+     * http://blog.csdn.net/xiao__gui/article/details/17298437?reload
+     * @param req
+     * @param resp
+     */
+    private void dealSession(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
+        System.out.println(session.getId());
+        if (session.getAttribute("user") == null) {
+            session.setAttribute("user", "xxg");
+            System.out.println("null");
+        } else {
+            System.out.println(session.getAttribute("user"));
+        }
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/com/aicai/service/login/dao/memberImp/MemberDaoImp.java
+++ src/main/java/com/aicai/service/login/dao/memberImp/MemberDaoImp.java
@@ -1,23 +1,20 @@
 package com.aicai.service.login.dao.memberImp;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.aicai.service.login.dao.MemberDao;
 import com.aicai.service.login.domain.Member;
-import com.aicai.service.login.domain.Social;
 
 @Repository
 public class MemberDaoImp implements MemberDao {
-	@Autowired
-	SqlSessionTemplate	sqlSessionTemplate;
+    //	@Autowired
+    //	SqlSessionTemplate	sqlSessionTemplate;
 	@Override
 	public Member login(String username, String md5pass) {
 		System.out.println("logindao");
-		String selectOne = "testNamespace.getTop1UserResultMap";
-		Social a = sqlSessionTemplate.selectOne(selectOne);
-		System.out.println(a);
+        //		String selectOne = "testNamespace.getTop1UserResultMap";
+        //		Social a = sqlSessionTemplate.selectOne(selectOne);
+        //		System.out.println(a);
 		return null;
 	}
 
